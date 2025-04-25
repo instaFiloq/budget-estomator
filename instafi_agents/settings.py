8eb6d1ca9116d4a74723dfd8b00032b17978cfb5
LokmanCode <lokman@approva.co>
2025-04-25 23:44:30 +0100

    update settings

--- instafi_agents/settings.py
+++ instafi_agents/settings.py
@@ -34,7 +34,7 @@ DEBUG = True
 APPEND_SLASH=False
 
 ALLOWED_HOSTS = [
-    'l*'
+    '*'
 ]
 
 
@@ -52,6 +52,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -136,6 +138,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
